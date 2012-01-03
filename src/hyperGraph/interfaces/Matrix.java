98335661601f6b01e5b93420bc9958311fbdd5ab
Tobi <Tobi@Tobi-NB>
2012-01-03 10:30:26 +0100

    HG: Matrix und MatrixImpl erweitert

--- src/hyperGraph/interfaces/Matrix.java
+++ src/hyperGraph/interfaces/Matrix.java
@@ -11,15 +11,15 @@ import java.util.Iterator;
  */
 public interface Matrix {
 
+	public int size();
 	public int width();
 	public int height();
 	public int get(int i, int j);
-	public Iterator<Integer> iterator(int i, int j);
+	//public Iterator<Integer> iterator(int i, int j);
 	public Matrix addColumn();
 	public Matrix addRow();
 	public Matrix removeColumn();
 	public Matrix removeRow();
-	
 	
 	
 }
