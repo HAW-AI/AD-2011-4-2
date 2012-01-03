b4653442842c3011383ad8329a4dbf80c69905b5
Stephan <stephan.berngruber@haw-hamburg.de>
2012-01-03 10:30:56 +0100

    HG: HyperGraph Interface added, HyperGraphImpl in progress...

--- src/hyperGraph/implementations/HyperGraphImpl.java
+++ src/hyperGraph/implementations/HyperGraphImpl.java
@@ -1,5 +1,42 @@
 package hyperGraph.implementations;
 
-public class HyperGraphImpl {
+import hyperGraph.interfaces.HyperGraph;
+
+public class HyperGraphImpl implements HyperGraph {
+
+	// public static void main(String[] args){
+	// System.out.println(nAHG);
+	//
+	// }
+
+	final public static HyperGraph emptyHyperGraph = new HyperGraphImpl();
+
+	private HyperGraphImpl() {
+	}
+
+
+	@Override
+	public HyperGraph addEdge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HyperGraph addVertex() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HyperGraph removeEdge() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HyperGraph removeVertex() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/hyperGraph/interfaces/HyperGraph.java
+++ src/hyperGraph/interfaces/HyperGraph.java
@@ -2,14 +2,47 @@ package hyperGraph.interfaces;
 
 public interface HyperGraph {
 
-	
 	public HyperGraph addEdge();
-	
+
 	public HyperGraph addVertex();
-	
+
 	public HyperGraph removeEdge();
-	
+
 	public HyperGraph removeVertex();
-	
-	
+
+	/**
+	 * @author Tobias Meurer
+	 * @author Stephan Berngruber
+	 * 
+	 */
+
+	final public HyperGraph nAHG = new NotAHyperGraph();
+
+	final public class NotAHyperGraph implements HyperGraph {
+
+		private NotAHyperGraph() {
+		};
+
+		@Override
+		public HyperGraph addEdge() {
+			return new NotAHyperGraph();
+		}
+
+		@Override
+		public HyperGraph addVertex() {
+			return new NotAHyperGraph();
+		}
+
+		@Override
+		public HyperGraph removeEdge() {
+			return new NotAHyperGraph();
+		}
+
+		@Override
+		public HyperGraph removeVertex() {
+			return new NotAHyperGraph();
+		}
+
+	}
+
 }
