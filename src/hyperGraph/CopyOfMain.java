c1d3592ade54641a9509f79d713c5d1e271cbff0
Stephan <stephan.berngruber@haw-hamburg.de>
2012-01-04 16:51:12 +0100

    HG: testing... testing

--- src/hyperGraph/CopyOfMain.java
+++ src/hyperGraph/CopyOfMain.java
@@ -16,7 +16,7 @@ public class CopyOfMain {
 		HyperGraph hyp1 = Values.emptyHyperGraph();
 		HyperGraph hyp2 = hyp1.addEdge();
 		System.out.println(hyp2);
-		//hyp1.addEdge(1);
+		// hyp1.addEdge(1);
 		hyp2 = hyp1.addVertex();
 		System.out.println(hyp2);
 		hyp1 = hyp2.addVertex();
@@ -25,8 +25,27 @@ public class CopyOfMain {
 		System.out.println(hyp2);
 		hyp1 = hyp2.addEdge();
 		System.out.println(hyp1);
-		hyp1 = hyp2.addEdge(0,1,2);
+		hyp1 = hyp2.addEdge(0, 1, 2);
+		System.out.println(hyp1);
+		hyp2 = hyp1.addEdge(0);
+		System.out.println(hyp2);
+		hyp1 = hyp2.addEdge(1);
+		System.out.println(hyp1);
+		hyp2 = hyp1.addEdge(2);
+		System.out.println(hyp2);
+		hyp1 = hyp2.addEdge();
+		System.out.println(hyp1);
+//		hyp1 = hyp2.addEdge(1,2,4);
+//		System.out.println(hyp1);
+		hyp1 = hyp2.addVertex();
+		System.out.println(hyp1);
+		hyp2 = hyp1.addVertex(1,3);
+		System.out.println(hyp2);
+//		hyp2 = hyp1.addVertex(1,3,4);
+//		System.out.println(hyp2);
+		hyp1 = hyp2.removeVertex(4);
 		System.out.println(hyp1);
 		
+
 	}
 }
