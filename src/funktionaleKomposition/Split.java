e58b1b30f5339eca0ceae9988a0cfad9354b694d
cyberaleks <aleksandr@nosov.de>
2012-01-05 15:46:21 +0100

    Split Interface hizugefügt

--- src/funktionaleKomposition/Split.java
+++ src/funktionaleKomposition/Split.java
@@ -1,68 +1,23 @@
-package funktionaleKomposition;
 
-import java.util.Arrays;
-
-
-public class Split {
-	private final String at;
-	private final boolean notempty;
-	private final boolean trim;
-	
-	private Split(String at, boolean notempty, boolean trim){
-		this.at=at;
-		this.notempty=notempty;
-		this.trim=trim;
-	}
+public interface Split {
 	/**
-	 * Gibt ein neues String Array ohne die Leeren Datensätze aus.
-	 * @param strArray String Array zum Berechnen
-	 * @return String[]
-	 */
-	private String[] doNotEmpty(String[] strArray){
-		if(!notempty) return strArray;
-		String[] newStrArray=new String[strArray.length];
-		int i=0;
-		for(String s: strArray)
-			if(!("".equals(s) || "".equals(s.trim()))) newStrArray[i++]=s;
-		return Arrays.copyOf(newStrArray, i);
-	}
-	/**
-	 * Führt die trim Operation auf jedes Element im Array aus. 
-	 * @return
-	 */
-	private String[] doTrim(String[] strArray){
-		if(!trim) return strArray;
-		String[] newStrArray=new String[strArray.length];
-		int i=0;
-		for(String s: strArray)
-			newStrArray[i++]=s.trim();
-		return Arrays.copyOf(newStrArray, i);
-	}
-	/**
-	 * Kopiert den Split Objekt und gibt ein neues Split Objekt zurück, 
+	 * Kopiert den Split Objekt und gibt ein neues Split Objekt zur√ºck, 
 	 * bei dem die Einstellung trim auf true gesetzt ist.
 	 * @return Split
 	 */
-	public Split trim(){
-		return new Split(at, notempty, true);
-	}
-	public Split notEmpty(){
-		return new Split(at, true, trim);
-	}
+	public Split trim();
 	/**
-	 * Konstrukt
-	 * @param at Das Zeichen an dem gesplitet wird.
-	 * @return
-	 */
-	public static Split at(String at){
-		return new Split(at, false, false);
-	}
+	 * Kopiert den Split Objekt und gibt ein neues Split Objekt zur√ºck, 
+	 * bei dem die Einstellung noempty auf true gesetzt ist.
+	 * @return Split
+	 */	
+	public Split notEmpty();
+
 	/**
-	 * Splitet den String und führt alle Funktionen auf den Array aus, die man eingestellt hat.
+	 * Splitet den String und f√ºhrt alle Funktionen auf den Array aus, die man eingestellt hat.
 	 * @param from String zum Spliten
 	 * @return
 	 */
-	public String[] from(String from){
-		return doTrim(doNotEmpty(from.split(at)));
-	}
+	public String[] from(String from);
+
 }
--- src/funktionaleKomposition/SplitTest.java
+++ src/funktionaleKomposition/SplitTest.java
@@ -9,18 +9,18 @@ public class SplitTest {
 
 	@Test
 	public void test() {
-		String[] strArray=Split.at(",").trim().notEmpty().from(" neue , ,funktionale , welt, ");
+		String[] strArray=SplitImpl.at(",").trim().notEmpty().from(" neue , ,funktionale , welt, ");
 		assertEquals("[neue, funktionale, welt]",Arrays.toString(strArray));
-		strArray=Split.at(",").trim().from(" neue , ,funktionale , welt, ");
+		strArray=SplitImpl.at(",").trim().from(" neue , ,funktionale , welt, ");
 		assertEquals("[neue, , funktionale, welt, ]",Arrays.toString(strArray));
-		strArray=Split.at(",").from(" neue , ,funktionale , welt, ");
+		strArray=SplitImpl.at(",").from(" neue , ,funktionale , welt, ");
 		assertEquals("[ neue ,  , funktionale ,  welt,  ]",Arrays.toString(strArray));
 		
-		strArray=Split.at("\n").from(" neue , , \n funktionale , welt, ");
+		strArray=SplitImpl.at("\n").from(" neue , , \n funktionale , welt, ");
 		System.out.println(" neue , , \n funktionale , welt, ");
 		System.out.println(Arrays.toString(strArray));
 
-		strArray=Split.at("||").trim().notEmpty().from(" neue   funktionale welt ");
+		strArray=SplitImpl.at("||").trim().notEmpty().from(" neue   funktionale welt ");
 		System.out.println(" neue , , funktionale , welt, ");
 		System.out.println(Arrays.toString(strArray));
 	}
